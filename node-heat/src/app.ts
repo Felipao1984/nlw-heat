3ab5bf9b3afaa730739f66e66e474dc73f478a48
Felipao1984 <felipe.silva84@protonmail.com>
2021-10-20 16:34:10 -0300

    First Commit

---
+++ node-heat/src/app.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import express from "express";
+
+
+const app = express();
+
+
+app.get('/github', (request, response)=>{
+    response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
+
+
+app.get('/signin/callback', (request, response)=>{
+
+    const { code } = request.query;
+    response.json({ code })
+
+})
+
+
+
+
+app.listen(4000, ()=> console.log("Running"))
